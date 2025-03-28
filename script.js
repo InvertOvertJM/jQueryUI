5e64610045e7c4f2315cdeda266bf524a9665a32
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-03-28 11:49:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-$(function() {
-    $("#datepicker").datepicker();
-});
+let signedOn = false;
+
 
 $(document).ready(function() {
     $(".nav-item").hover(
         function() {
-            $(this).css("color", "blue");
+            $(this).css("color", "lightblue");
         }, 
         function() {
             $(this).css("color", "white");
         }
     );
+
+    $(function() {
+        $("#datepicker").datepicker();
+    });
+
+    $( function() {
+        $( "#progressbar" ).progressbar({
+          value: 20
+        });
+      } );
+
+      
 });
+
