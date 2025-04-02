94b179acd4815a78da7e1d9280d83f0069edf3b1
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-02 11:40:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let username;
 let password;
 let from;
 let to;
-
+let progress1 = 0;
+let progress2 = 0;
 
 $(document).ready(function() {
     $(".nav-item").hover(
@@ -17,13 +18,71 @@ $(document).ready(function() {
         $("#datepicker").datepicker();
 
         $( "#progress-bar1" ).progressbar({
-          value: 0
+          value: progress1
       } );
-
+      
       $( "#progress-bar2" ).progressbar({
-        value: 0
+        value: progress2
     } );
 
-    $( "#dialog" ).dialog();
+    $( "#dialog1" ).dialog({ autoOpen: false });
+//AFTER INITIALIZATION
 });
+
+
+
+    $( "#sign-up-btn" ).click(function() {
+        username = $("#new-username").val();
+        password = $("#new-password").val();
+        signedOn = true;
+        if($("#new-username").val() == "" || $("#new-password").val() == "" || $("#new-email").val() == ""){
+            $( "#dialog1" ).dialog( "open" );
+        }
+});
+
+    $("#new-username").change(function(){
+        if($("#new-username").val() != ""){
+            progress2 += 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+        else{
+            progress2 -= 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+    });
+
+    $("#new-password").change(function(){
+        if($("#new-password").val() != ""){
+            progress2 += 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+        else{
+            progress2 -= 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+    });
+
+    $("#new-email").change(function(){
+        if($("#new-email").val() != ""){
+            progress2 += 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+        else{
+            progress2 -= 35;
+            $( "#progress-bar2" ).progressbar({
+                value: progress2
+            } );
+        }
+    });
+
 
